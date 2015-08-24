0335f30b6000da05bad467134523cd3138dd28e8
Sergio Revilla <replymusic@gmail.com>
2015-08-24 11:38:44 +0200

    using mongo

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
+require 'mongo'
 
 class App < Sinatra::Base
 
+	before do
+		@db = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'polyglot')
+	end
+
 	get '/' do
+		@db[:test].insert_one({ name: 'Clotxinismo' })
 		"It works!"
 	end
 end
