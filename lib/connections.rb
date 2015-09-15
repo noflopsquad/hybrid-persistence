af7a6994f70c38d5019b15324bb36f6df6207f49
Victor Miguez <victor.miguez.martinez@gmail.com>
2015-09-15 20:11:53 +0200

    mongo log level to ERROR

--- lib/connections.rb
+++ lib/connections.rb
@@ -5,6 +5,7 @@ class Connections
 	@@sql = SQLite3::Database.new('sqlite.db')
   @@sql.results_as_hash = true
   
+  Mongo::Logger.logger.level = Logger::ERROR
   @@mongo = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'mixed')
 
   def self.sql
