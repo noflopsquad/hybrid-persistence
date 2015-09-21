fe600b3ede7fce3530b959da86ecfe735634534a
trikitrok <trikitrok@gmail.com>
2015-09-21 13:14:47 +0200

    Removed unnecessary fix to value object

--- lib/mixed/address_identity.rb
+++ lib/mixed/address_identity.rb
@@ -4,8 +4,4 @@ require 'digest/sha1'
 class AddressIdentity
   extend ValueObject
   fields :street_name, :street_address
-
-  def hash
-    Digest::SHA1.hexdigest(self.street_name + " " + self.street_address)
-  end
 end
--- lib/mixed/person_identity.rb
+++ lib/mixed/person_identity.rb
@@ -4,8 +4,4 @@ require 'digest/sha1'
 class PersonIdentity
   extend ValueObject
   fields :first_name, :last_name
-
-  def hash
-    Digest::SHA1.hexdigest(self.first_name + " " + self.last_name)
-  end
 end
